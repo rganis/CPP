b4000351a8350689d11cd72ab219574c994e08a5
rganis <rganis@wgu.edu>
2023-04-17 21:43:26 -0400

    add string to degree.h enum type

--- degree.h
+++ degree.h
@@ -1,1 +1,2 @@
 enum DegreeProgram{ SECURITY, NETWORK, SOFTWARE }; // enumerated data types for the degree programs
+static const std::string degreeProgramStrings[] = { "SECURITY", "NETWORK", "SOFTWARE" }; // array of strings for the degree programs
